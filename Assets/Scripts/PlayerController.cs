64f866d8643008b54657afecc6f519132cd83234
Luis Rogelio Reyes <luisrogelio98@gmail.com>
2020-08-10 10:38:40 -0400

    camera follow and animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float speed = 4.0f;
+
+    private bool isWalking = false;
+
+    public Vector2 lastMovement = Vector2.zero;
 
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
+
+    private const string lastHorizontal = "LastHorizontal";
+
+    private const string lastVertical = "LastVertical";
+
+    private const string walking = "isWalking";
 
     private Animator animator;
 
@@ -18,29 +27,40 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // s = v * t
+        isWalking = false;
         /*
             control de movimiento del usuario
         */
+        // movimiento horizontal
         if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
             this.transform.Translate(
                 new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0)
             );
+            isWalking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
-
+        // movimiento vertical
         if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
             this.transform.Translate(
                 new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0)
             );
+            isWalking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
         /*
             sincronizando con el grafo de animaciones
         */
         animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
         animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+
+        animator.SetBool(walking, isWalking);
+
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+
+        animator.SetFloat(lastVertical, lastMovement.y);
     }
 }
