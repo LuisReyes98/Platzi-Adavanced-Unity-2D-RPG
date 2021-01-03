92ef7f12dd0365d05a615e75f73bafdc38d59f79
Luis Rogelio Reyes <luisrogelio98@gmail.com>
2021-01-02 23:55:49 -0400

    player weapong animations

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class WeaponDamage : MonoBehaviour
 {
-    
+    public int damage;
+
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
-            Destroy(collision.gameObject);
+            // Destroy(collision.gameObject);
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
         }
     }
 }
